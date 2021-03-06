cffb04130ced1222dc275cd86b8728accdae7798
vijay gohel <gohil5898@gmail.com>
2021-03-06 11:37:11 +0530

    initial again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'databinding-demo';
+  serverElements = [{type:'server',name:'testserver',content:'this is a test!'}];
+  
+
+  
 }
